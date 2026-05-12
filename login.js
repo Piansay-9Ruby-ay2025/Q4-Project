5e7cbd1866f25f1526e426c250efabebaa90e439
veran-9r-ay2526 <veran.ls@obmontessori.edu.ph>
2026-05-12 13:12:28 +0800

    UPDATE 1

---
+++ login.js
@@ -0,0 +1,27 @@
+function showLoginHelp() {
+    alert("Username is Student48 and Password is Peroxide");
+}
+function login() {
+    const username = document.getElementById("usernameInp").value;
+    const password = document.getElementById("passwordInp").value;
+    const messageElem = document.getElementById("loginMessage");
+
+    switch (true) {
+        case (username === "Student48" && password === "Peroxide"):
+            messageElem.textContent = "Login successful! Welcome, " + username + ".";
+            messageElem.style.color = "green";
+            window.location.href = "notebook.html";
+            break;
+        default:
+            messageElem.textContent = "Incorrect Username or Password. Please try again.";
+            messageElem.style.color = "red";
+    }
+}
+function toggleVisi(){
+    const passwordInput = document.getElementById("passwordInp");
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+    } else {
+        passwordInput.type = "password";
+    }
+}
